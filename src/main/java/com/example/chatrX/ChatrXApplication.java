9aa24ff1b8adb04b2234d0cf9655cfc6b59eeef6
praveen <238w1a1285@vrsec.ac.in>
2025-11-14 22:59:46 +0530

    first commit

---
+++ src/main/java/com/example/chatrX/ChatrXApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatrX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatrXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatrXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatrX/ChatrXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatrX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatrXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
